5836a8cfbe90c23c010cbef50f285c8a25af36d8
ljyockey <leoyockey@gmail.com>
2018-12-31 18:32:23 -0800

    code cleanup

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ import './styles/main.scss'
 export class App extends Component {
     render () {
         return (
-            <div class='app-placeholder'>
-                <Hello subject={'world'} />
-            </div> 
+            <Hello subject={'world'} />
         )
     }
 }
